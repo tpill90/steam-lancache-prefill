2e4f415b7441a1853c249dde07a9e2db2c351830
Tim Pilius <tpilius@gmail.com>
2022-06-30 16:51:12 -0400

    asd

--- DepotDownloader/Handlers/ManifestHandler.cs
+++ DepotDownloader/Handlers/ManifestHandler.cs
@@ -3,10 +3,10 @@ using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using DepotDownloader.Exceptions;
 using DepotDownloader.Models;
 using DepotDownloader.Protos;
 using DepotDownloader.Steam;
-using Spectre.Console;
 using SteamKit2;
 
 namespace DepotDownloader.Handlers
@@ -14,18 +14,23 @@ namespace DepotDownloader.Handlers
     //TODO document
     public class ManifestHandler
     {
-        private readonly IAnsiConsole _ansiConsole;
         private readonly CdnPool _cdnPool;
         private readonly Steam3Session _steam3Session;
 
-        public ManifestHandler(IAnsiConsole ansiConsole, CdnPool cdnPool, Steam3Session steam3Session)
+        public ManifestHandler(CdnPool cdnPool, Steam3Session steam3Session)
         {
-            _ansiConsole = ansiConsole;
             _cdnPool = cdnPool;
             _steam3Session = steam3Session;
         }
 
-        //TODO document
+        //TODO test exception handling here, and in the calling method
+        /// <summary>
+        /// Requests a depot's manifest from Steam's servers, or if it has been requested before,
+        /// it will load the manifest from disk.
+        /// </summary>
+        /// <param name="depot">The depot to download a manifest for</param>
+        /// <returns>A manifest file</returns>
+        /// <exception cref="ManifestException">Throws if no manifest was returned by Steam</exception>
         public async Task<ProtoManifest> GetManifestFile(DepotInfo depot)
         {
             if (File.Exists(depot.ManifestFileName))
@@ -61,9 +66,7 @@ namespace DepotDownloader.Handlers
             }
             if (manifest == null && retryCount == 5)
             {
-                //TODO better exception type
-                //TODO how should this be handled by functions that call this?  
-                throw new Exception($"Unable to download manifest for depot {depot.Name} - {depot.DepotId}");
+                throw new ManifestException($"Unable to download manifest for depot {depot.Name} - {depot.DepotId}.  Manifest request received no response.");
             }
             
             var protoManifest = new ProtoManifest(manifest, depot);
@@ -72,27 +75,26 @@ namespace DepotDownloader.Handlers
             return protoManifest;
         }
 
-        // TODO document
-        // TODO include in docs -  ManifestRequestCodes can only be requested if you own the game. They act as a form of "authorization" for the CDN.
-        // TODO  https://steamdb.info/blog/manifest-request-codes/ 
         /// <summary>
         /// Requests a ManifestRequestCode for the specified depot.  Each depot will have a unique code, that gets rotated every 5 minutes.
         /// These manifest codes are not unique to a user, so they will be used by all users in the same 5 minute window.
         ///
         /// These manifest codes act as a form of "authorization" for the CDN.  You can only download a manifest if your account has access to the
         /// specified depot, so since the CDN itself doesn't check for access, this will prevent unauthorized depot downloads
+        ///
+        /// https://steamdb.info/blog/manifest-request-codes/ 
         /// </summary>
-        /// <param name="depot">The depot </param>
-        /// <returns></returns>
+        /// <param name="depot">The depot to request a manifest code for</param>
+        /// <returns>A manifest code valid for 5 minutes.</returns>
+        /// <exception cref="ManifestException">Throws if no valid manifest code was found</exception>
         private async Task<ManifestRequestCode> GetManifestRequestCode(DepotInfo depot)
         {
             ulong manifestRequestCode = await _steam3Session.steamContent.GetManifestRequestCode(depot.DepotId, depot.ContaingAppId, depot.ManifestId.Value, "public");
             
-            // If we could not get the manifest code, this is a fatal error
+            // If we could not get the manifest code, this is a fatal error, as it we can't download the manifest without it.
             if (manifestRequestCode == 0)
             {
-                //TODO handle error here
-                _ansiConsole.WriteLine($"No manifest request code was returned for {depot.DepotId} {depot.ManifestId.Value}");
+                throw new ManifestException($"No manifest request code was returned for {depot.DepotId} {depot.ManifestId.Value}");
             }
 
             return new ManifestRequestCode
