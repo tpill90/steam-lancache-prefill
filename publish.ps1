c017be2d5d6c9914d7ccdcf6f41ac463e61d3d60
Tim Pilius <tpilius@gmail.com>
2022-07-19 13:43:25 -0400

    asd

--- publish.ps1
+++ publish.ps1
@@ -7,8 +7,8 @@ Remove-Item publish -Recurse -Force -ErrorAction SilentlyContinue
 foreach($runtime in @("win-x64"))
 {
     Write-Host "Publishing $runtime" -ForegroundColor Cyan
-    dotnet publish .\BattleNetPrefill\BattleNetPrefill.csproj `
-    -o publish/BattleNetPrefill-$runtime `
+    dotnet publish .\SteamPrefill\SteamPrefill.csproj `
+    -o publish/SteamPrefill-$runtime `
     -c Release `
     --runtime $runtime `
     --self-contained true `
@@ -16,9 +16,9 @@ foreach($runtime in @("win-x64"))
     /p:PublishReadyToRun=true `
     /p:PublishTrimmed=true
 
-    Compress-Archive -path publish/BattleNetPrefill-$runtime publish/$runtime.zip
+    Compress-Archive -path publish/SteamPrefill-$runtime publish/$runtime.zip
 
-    $folderSize = "{0:N2} MB" -f((Get-ChildItem publish/BattleNetPrefill-$runtime | Measure-Object -Property Length -sum).sum / 1Mb)
+    $folderSize = "{0:N2} MB" -f((Get-ChildItem publish/SteamPrefill-$runtime | Measure-Object -Property Length -sum).sum / 1Mb)
     Write-Host "Published file size : " -NoNewline
     Write-Host -ForegroundColor Cyan $folderSize
 
@@ -31,17 +31,17 @@ foreach($runtime in @("win-x64"))
 foreach($runtime in @("linux-x64", "osx-x64"))
 {
     Write-Host "\n\nPublishing $runtime" -ForegroundColor Cyan
-    dotnet publish .\BattleNetPrefill\BattleNetPrefill.csproj `
-    -o publish/BattleNetPrefill-$runtime `
+    dotnet publish .\SteamPrefill\SteamPrefill.csproj `
+    -o publish/SteamPrefill-$runtime `
     -c Release `
     --runtime $runtime `
     --self-contained true `
     /p:PublishSingleFile=true `
     /p:PublishTrimmed=true
 
-    $folderSize = "{0:N2} MB" -f((Get-ChildItem publish/BattleNetPrefill-$runtime | Measure-Object -Property Length -sum).sum / 1Mb)
+    $folderSize = "{0:N2} MB" -f((Get-ChildItem publish/SteamPrefill-$runtime | Measure-Object -Property Length -sum).sum / 1Mb)
     Write-Host "Published file size : " -NoNewline
     Write-Host -ForegroundColor Cyan $folderSize
 
-    Compress-Archive -path publish/BattleNetPrefill-$runtime publish/$runtime.zip
+    Compress-Archive -path publish/SteamPrefill-$runtime publish/$runtime.zip
 }
