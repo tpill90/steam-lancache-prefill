453dc2effb9b8765721ccceefc82216a938aaffb
Tim Pilius <tpilius@gmail.com>
2022-07-28 15:53:54 -0400

    Filtering out games that are categorized as a mod, as well as AMD driver updates.

---
+++ SteamPrefill/Models/Enums/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamPrefill.Models.Enums
+{
+    public enum Category
+    {
+        Mods_HL2 = 6,
+        Mods = 19
+    }
+}
---
+++ SteamPrefill/Models/Enums/ExcludedAppId.cs
@@ -0,0 +1,12 @@
+﻿namespace SteamPrefill.Models.Enums
+{
+    public enum ExcludedAppId
+    {
+        // Spacewar is a non-playable game, that is required for Steamworks multiplayer functionality to work
+        SpaceWar = 480,
+        PCGamerOnline = 92500,
+        RisingStormBetaDedicatedServer = 238690,
+        MinervaMetastasis = 235780,
+        GamepadControllerTemplate = 1456390,
+    }
+}
