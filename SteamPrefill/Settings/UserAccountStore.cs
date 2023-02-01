91997189d61f944cd5ba534b9db49bef2b9d7adf
Tim Pilius <tpilius@gmail.com>
2023-02-01 16:21:57 -0500

    SessionIds will now be randomized, so that multiple instances of SteamPrefill no longer log each other out.

--- SteamPrefill/Settings/UserAccountStore.cs
+++ SteamPrefill/Settings/UserAccountStore.cs
@@ -3,7 +3,7 @@
     /// <summary>
     /// Keeps track of the session tokens returned by Steam, that allow for subsequent logins without passwords.
     /// </summary>
-    [ProtoContract]
+    [ProtoContract(SkipConstructor = true)]
     public sealed class UserAccountStore
     {
         /// <summary>
@@ -13,23 +13,33 @@
         [ProtoMember(1)]
         public Dictionary<string, byte[]> SentryData { get; private set; }
 
-        //TODO rename to session key
         /// <summary>
         /// Upon a successful login to Steam, a "Login Key" will be returned to use on subsequent logins.
         /// This login key can be considered a "session token", and can be used on subsequent logins to avoid entering a password.
         /// These keys will be unique to each user.
         /// </summary>
         [ProtoMember(2)]
-        public Dictionary<string, string> LoginKeys { get; private set; }
+        public Dictionary<string, string> SessionTokens { get; private set; }
 
         //TODO can I restrict using this getter? since there is already a method
         [ProtoMember(3)]
         public string CurrentUsername { get; private set; }
 
+        /// <summary>
+        /// Used to identify separate instances of Steam/SteamPrefill on the Steam network.
+        /// As long as these don't collide, multiple instances can be logged in without logging each other out.
+        /// </summary>
+        [ProtoMember(4)]
+        public uint? SessionId { get; private set; }
+
+        [SuppressMessage("Security", "CA5394:Random is an insecure RNG", Justification = "Security doesn't matter here, as all that is needed is a unique id.")]
         private UserAccountStore()
         {
             SentryData = new Dictionary<string, byte[]>();
-            LoginKeys = new Dictionary<string, string>();
+            SessionTokens = new Dictionary<string, string>();
+
+            var random = new Random();
+            SessionId = (uint)random.Next(0, 16384);
         }
 
         public async Task<string> GetUsernameAsync(IAnsiConsole ansiConsole)
@@ -65,7 +75,8 @@
             }
 
             using var fileStream = File.Open(AppConfig.AccountSettingsStorePath, FileMode.Open, FileAccess.Read);
-            return Serializer.Deserialize<UserAccountStore>(fileStream);
+            var userAccountStore = Serializer.Deserialize<UserAccountStore>(fileStream);
+            return userAccountStore;
         }
 
         public void Save()
