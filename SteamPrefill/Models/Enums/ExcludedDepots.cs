6d270211ad62a4eefba72c68e717007db8d67848
Tim Pilius <tpilius@gmail.com>
2024-01-18 17:12:28 -0500

    Manually excluding Counter Strike 2 depots that are unused by the actual Steam client.  For whatever reason Steam is able to determine that these apps are not needed, however there is no supporting App metadata that correlates to this filter.

--- SteamPrefill/Models/Enums/ExcludedDepots.cs
+++ SteamPrefill/Models/Enums/ExcludedDepots.cs
@@ -8,11 +8,20 @@
             // Steam somehow determines that this depot should not be installed alongside Dota 2, however there is no associated metadata
             // that indicates that this depot should not be downloaded.  This must be hardcoded in the real Steam client as well.
             381450,
+            // CS2 Workshop Tools, also unclear how Valve is filtering this from installing normally
+            2347779,
 
             // VTOL VR - https://steamdb.info/depot/1770480/
             // Manually excluding this depot as it is currently impossible to request a manifest code for it, thus it shouldn't be downloaded.
             // I'm currently unable to determine how the real Steam client determines to skip this depot.
-            1770480
+            1770480,
+
+            // CS2 excludes these for some unknown reason.  Might be related to "systemdefined"	on the DepotInfo
+            731,
+            732,
+            734,
+            
+            //TODO should I exclude the directx runtimes from the downloads?  Kind of adds noise for no real reason
         };
     }
 }
