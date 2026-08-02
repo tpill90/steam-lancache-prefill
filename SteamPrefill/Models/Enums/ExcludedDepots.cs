a7d68e664f4bd942d6301d4a65bbee6e0a0b68d6
Tim Pilius <tpilius@gmail.com>
2026-08-02 17:12:41 -0400

    asd

--- SteamPrefill/Models/Enums/ExcludedDepots.cs
+++ SteamPrefill/Models/Enums/ExcludedDepots.cs
@@ -13,11 +13,6 @@
             // CS2 Workshop Tools, also unclear how Valve is filtering this from installing normally
             2347779,
 
-            // VTOL VR - https://steamdb.info/depot/1770480/
-            // Manually excluding this depot as it is currently impossible to request a manifest code for it, thus it shouldn't be downloaded.
-            // I'm currently unable to determine how the real Steam client determines to skip this depot.
-            1770480,
-
             // CS2 excludes these for some unknown reason.  Might be related to "systemdefined"	on the DepotInfo
             731,
             732,
