d1790abb57ee9dd17f62bdcfff9f8ed65b7e27c8
Tim Pilius <tpilius@gmail.com>
2026-08-24 20:04:32 -0400

    Fixing dotnet format issue

--- SteamPrefill/Models/DepotInfo.cs
+++ SteamPrefill/Models/DepotInfo.cs
@@ -38,7 +38,7 @@
         private uint? DlcAppId { get; }
 
         // If there is no manifest we can't download this depot, and if there is no shared depot then we can't look up a related manifest we could use. Also invalidate self-linking depots from somce DLCs that can't be downloaded.
-        public bool IsInvalidDepot => ManifestId == null && (DepotFromApp == null || DepotFromApp.Value == DepotId); 
+        public bool IsInvalidDepot => ManifestId == null && (DepotFromApp == null || DepotFromApp.Value == DepotId);
 
         public List<OperatingSystem> SupportedOperatingSystems { get; init; } = new List<OperatingSystem>();
         public Architecture Architecture { get; init; }
